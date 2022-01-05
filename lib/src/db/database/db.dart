e78681a9bd3bc1f019f7d4177b32ff3c69455717
Devin <linux_redhat@sina.com>
2022-01-05 15:19:48 +0800

    lib_widget提交

--- lib/src/db/database/db.dart
+++ lib/src/db/database/db.dart
@@ -1,10 +1,10 @@
-import 'package:floor/floor.dart';
-import 'package:lib_core/src/db/dao/user_dao.dart';
-import 'package:lib_core/src/db/entity/user.dart';
-
-part 'db.g.dart';
-
-@Database(version: 1, entities: [User])
-abstract class DataBase extends FloorDatabase {
-  UserDao get userDao;
-}
+// import 'package:floor/floor.dart';
+// import 'package:lib_core/src/db/dao/user_dao.dart';
+// import 'package:lib_core/src/db/entity/user.dart';
+//
+// part 'db.g.dart';
+//
+// @Database(version: 1, entities: [User])
+// abstract class DataBase extends FloorDatabase {
+//   UserDao get userDao;
+// }
