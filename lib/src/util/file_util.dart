2533c6f658651f4deeb2375db667438519883bcf
Devin <linux_redhat@sina.com>
2023-04-23 16:10:27 +0800

    新增download path

--- lib/src/util/file_util.dart
+++ lib/src/util/file_util.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:path_provider/path_provider.dart';
+import 'package:android_path_provider/android_path_provider.dart';
 
 class FileUtil {
 
@@ -12,5 +15,24 @@ class FileUtil {
     final directory = await getTemporaryDirectory();
     print('temp dir path: ${directory.path}');
     return directory.path;
+  }
+
+  static Future<String> get downloadPath async {
+    String externalStorageDirPath;
+
+    if (Platform.isAndroid) {
+      try {
+        externalStorageDirPath = await AndroidPathProvider.downloadsPath;
+      } catch (err, st) {
+        print('failed to get downloads path: $err, $st');
+
+        final directory = await getExternalStorageDirectory();
+        externalStorageDirPath = directory?.path??'';
+      }
+    } else {
+      externalStorageDirPath =
+          (await getApplicationDocumentsDirectory()).absolute.path;
+    }
+    return externalStorageDirPath;
   }
 }
